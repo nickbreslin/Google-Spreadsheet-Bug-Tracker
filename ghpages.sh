07401122081a858cabd3b944522b3b0a725e3992
Nick Breslin <nickbreslin@gmail.com>
2015-11-27 14:57:52 -0800

    ghpages

--- ghpages.sh
+++ ghpages.sh
@@ -1,6 +1,6 @@
-rm -rf docs/
-vendor/bin/phpdoc -d src/ -t docs
+vendor/bin/phpdoc -d src/ -t docs_new
 git checkout gh-pages
+cp -r docs_new docs
 git add docs/
 git commit -m 'Documentation Sync'
 git push origin gh-pages
